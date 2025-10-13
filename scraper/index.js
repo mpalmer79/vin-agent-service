620a1b6ff5369665cb5c7404c186cb3ba347b1c8
mpalmer79 <mpalm2025@gmail.com>
2025-10-13 08:48:26 -0400

    Update index.js

--- scraper/index.js
+++ scraper/index.js
@@ -28,13 +28,16 @@ async function scrapeVINInventory() {
     console.log('🌐 Launching browser...');
     browser = await puppeteer.launch({
       headless: 'new',
+      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium-browser',
       args: [
         '--no-sandbox',
         '--disable-setuid-sandbox',
         '--disable-dev-shm-usage',
         '--disable-gpu',
         '--disable-software-rasterizer',
-        '--disable-extensions'
+        '--disable-extensions',
+        '--no-first-run',
+        '--disable-default-apps'
       ]
     });
     
